b33306e442a87198cb0e7dfa67d5c2b2ea928704
Andraž Kos <adonis.inzeniring@gmail.com>
2016-06-19 21:15:36 +0200

    slovenščina

--- tests/index.js
+++ tests/index.js
@@ -1,28 +1,41 @@
-import React from 'react';
-import { createRenderer } from 'react-addons-test-utils';
-import createComponent from 'react-unit';
-import tape from 'tape';
-import addAssertions from 'extend-tape';
-import jsxEquals from 'tape-jsx-equals';
-const test = addAssertions(tape, {jsxEquals});
+import React from 'react'
+import createComponent from 'react-unit'
+import tape from 'tape'
+
+import { createRenderer } from 'react-addons-test-utils'
+import addAssertions from 'extend-tape'
+import jsxEquals from 'tape-jsx-equals'
+
+const test = addAssertions(tape, {jsxEquals})
 
 // Component to test
-import Button from '../components/Button';
+import Button from '../components/Button'
 
-test('----- React Component Tests: Button -----', (t) => {
-
+test('Button props', t => {
   // Shallow rendering: Render React element only *one* level deep
-  const component = createComponent.shallow(<Button label="share" />);
+  const component = createComponent.shallow(<Button label='foo' />)
 
   // Test component props and content
-  t.equal(component.props.className, 'default-class', 'ClassName props of component should equal "share"');
-  t.equal(component.text, 'share', 'Label props of component should be rendered as Button text "share"');
+  t.equal(
+    component.props.className,
+    'default-class',
+    'props.className mora uporabiti default če ni definiran')
 
+  t.equal(component.text,
+    'foo',
+    'props.label se mora rendrati kot vsebina')
+
+  t.end()
+})
+
+test('Button render', t => {
   // Test rendered output
-  const renderer = createRenderer();
-  renderer.render(<Button label="share" />);
-  const result = renderer.getRenderOutput();
-  t.jsxEquals(result, <div className="default-class">share</div>);
+  const r = createRenderer()
+  r.render(<Button label='share' />)
+  const result = r.getRenderOutput()
+  t.jsxEquals(result, <div className='default-class'>
+                        share
+                      </div>)
 
-  t.end();
-});
+  t.end()
+})
